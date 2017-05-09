9d26bfeb77083bb39eb2398ec809da2a529827b2
Cole Furfaro-Strode <colestrode@gmail.com>
2017-05-09 00:09:50 -0400

    working slapp app

---
+++ index.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const server = require('./lib/server')
+
+const slapp = require('slapp')({
+  convo_store: require('./lib/convo-store'),
+  context: require('./lib/context'),
+  log: true,
+  colors: true,
+  ignoreSelf: true,
+  ignoreBots: true
+})
+
+slapp.message('rick', 'ambient', (msg) => {
+  request({
+    uri: 'http://rickandmortyquotes.eu-central-1.elasticbeanstalk.com',
+    method: 'GET',
+    json: true
+  }, (err, res, body) => {
+    if (err || res.statusCode >= 400) {
+      return console.log('rick and morty error', err || body)
+    }
+
+    if (res.statusCode === 200) {
+      msg.say(body.what)
+    }
+  })
+})
+
+slapp.attachToExpress(server, {
+  event: true,
+  command: true,
+  action: true
+}).listen(process.env.PORT, () => console.log(`server listening on ${process.env.PORT}`))
